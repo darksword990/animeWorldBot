742dd2a62ea1335f154ebbe1214e477d214c86cc
darksword990 <71089813+darksword990@users.noreply.github.com>
2021-02-15 17:50:32 +0500

    hi

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -7,8 +7,8 @@ module.exports = {
     examples: ['@Crawler Being abusive', '76832545763724554 Being abusive'],
     run: async (client, message, args, prefix) => {
         if (!args.length) return;
-        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-        if (member) {
+        const member = message.mentions.members.array() || message.guild.members.cache.get(args[0])
+        if (member.length != 0 || member) {
             const botRole = client.guilds.cache.get(message.guild.id).member(client.user.id).roles.highest.position
             if (botRole <= member.roles.highest.position) return message.channel.send(`Bot must be higher than the mentioned member!`);
             if (message.member.roles.highest.position <= member.roles.highest.position && !message.guild.owner) return message.channel.send(`You can't ban a person higher than you!`);
@@ -17,7 +17,9 @@ module.exports = {
             if (member.user.bot) return message.channel.send(`You can't ban the bot!`);
             args.shift()
             message.channel.send(`${member.user.tag} has been banned!`)
-            await member.ban({days: 7, reason: `${message.member.user.tag}: ${args.join(" ")}`})
+            member.forEach(async m => {
+                await m.ban({days: 7, reason: `${message.member.user.tag}: ${args.join(" ")}`})
+            }) || await member.ban({days: 7, reason: `${message.member.user.tag}: ${args.join(" ")}`})
         }
     }
 }
