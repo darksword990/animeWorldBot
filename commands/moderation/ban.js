d68c5e3b02be613094814accc839a0772c9d0397
darksword990 <71089813+darksword990@users.noreply.github.com>
2021-02-15 18:09:38 +0500

    hi

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -8,7 +8,7 @@ module.exports = {
     run: async (client, message, args, prefix) => {
         if (!args.length) return;
         const member = message.mentions.members.array() || message.guild.members.cache.get(args[0])
-        if (member) {
+        if (!member.isArray()) {
             const botRole = client.guilds.cache.get(message.guild.id).member(client.user.id).roles.highest.position
             if (botRole <= member.roles.highest.position) return message.channel.send(`Bot must be higher than the mentioned member!`);
             if (message.member.roles.highest.position <= member.roles.highest.position && !message.guild.owner) return message.channel.send(`You can't ban a person higher than you!`);
