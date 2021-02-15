942700b0d7478d9758404767d7c0fab76dcd2b8a
darksword990 <71089813+darksword990@users.noreply.github.com>
2021-02-15 18:04:04 +0500

    hi

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -8,7 +8,7 @@ module.exports = {
     run: async (client, message, args, prefix) => {
         if (!args.length) return;
         const member = message.mentions.members.array() || message.guild.members.cache.get(args[0])
-        if (member.length != 0 || member) {
+        if (member) {
             const botRole = client.guilds.cache.get(message.guild.id).member(client.user.id).roles.highest.position
             if (botRole <= member.roles.highest.position) return message.channel.send(`Bot must be higher than the mentioned member!`);
             if (message.member.roles.highest.position <= member.roles.highest.position && !message.guild.owner) return message.channel.send(`You can't ban a person higher than you!`);
@@ -17,9 +17,21 @@ module.exports = {
             if (member.user.bot) return message.channel.send(`You can't ban the bot!`);
             args.shift()
             message.channel.send(`${member.user.tag} has been banned!`)
-            member.forEach(async m => {
-                await m.ban({days: 7, reason: `${message.member.user.tag}: ${args.join(" ")}`})
-            }) || await member.ban({days: 7, reason: `${message.member.user.tag}: ${args.join(" ")}`})
+            await member.ban({days: 7, reason: `${message.member.user.tag}: ${args.join(" ")}`})
+        } else if (Array.isArray(member)) {
+            if (member.length != 0) {
+                member.forEach(async m => {
+                    const botRole = client.guilds.cache.get(message.guild.id).member(client.user.id).roles.highest.position
+                    if (botRole <= m.roles.highest.position) return message.channel.send(`Bot must be higher than the mentioned member!`);
+                    if (message.member.roles.highest.position <= m.roles.highest.position && !message.guild.owner) return message.channel.send(`You can't ban a person higher than you!`);
+                    if (m.id == message.guild.ownerID) return message.channel.send(`You can't ban owner!`);
+                    if (message.member.id == m.id) return message.channel.send(`You can't ban yourself!`);
+                    if (m.user.bot) return message.channel.send(`You can't ban the bot!`);
+                    args.shift()
+                    message.channel.send(`${m.user.tag} has been banned!`)
+                    await m.ban({days: 7, reason: `${message.member.user.tag}: ${args.join(" ")}`})
+                })
+            }
         }
     }
 }
